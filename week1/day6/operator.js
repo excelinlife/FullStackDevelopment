725bd8a901a9f59253bf872ad5f90b53e22f10f6
Ashish <barnwal.ashish309@gmail.com>
2022-04-22 20:03:11 +0530

    updated code for day 6

---
+++ week1/day6/operator.js
@@ -0,0 +1,19 @@
+function action(){
+    alert('you have clicked on the button...');
+    console.log('you have clicked');
+    var variable = 'ashish'
+    document.write(variable +" ");
+    var first =90;
+    var second =20;
+    var result = first-second;
+
+    var a=false;
+    var b=true;
+document.write(a&&b);
+    document.write(first%second);
+    ocument.write(first==second);
+    ocument.write(first>second);
+    ocument.write(first<second);
+
+}
+
---
+++ week1/day6/practice.js
@@ -0,0 +1,20 @@
+        function action(){
+            alert('you have clicked on the button...');
+            console.log('you have clicked');
+            console.log('we are going to show hi on page');
+            var biryani='Vinay';
+            document.write(biryani +" ");
+            muttonKabab='Hashita';
+            const constant =200;
+            let newNumber = 300;
+            document.write(muttonKabab +" ");
+            variable='Ashish';
+            document.write(biryani +" ");
+            document.write(constant+ " ");
+            document.write(newNumber);
+            newNumber=500;
+            document.write(newNumber +" ");
+            biryani='Pankaj'
+            biryani='Harshita';
+            document.write(biryani +" ");
+        }
